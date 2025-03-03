d820866981362d0f76bbde64023ee86dc2867532
Taimoor Ali <taimoorali083@gmail.com>
2025-03-03 09:34:54 +0500

    this.setState use in class

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react'
 import './App.css'
 import UserList from './UserList'
 export default class App extends Component {
-  
+  constructor(){
+    super()
+    this.state = {
+      name : "Taimoor",
+      age:30
+    }
+  }
   render() {
+    const {name,age} = this.state
     return (
       <div className='App'>
         <h1>Props in Class Component</h1>
-        <UserList name="Abdul Ahad " email="Taimoorali083@gmail.c0m"/>  
+        {/* <UserList name="Abdul Ahad " email="Taimoorali083@gmail.c0m"/>   */}
+        <button onClick={()=>{this.setState({name:"Ali",age:19})}}>Click to change data</button>
+        <UserList name={name} age={age}/>  
+
       </div>
     )
   }
