0478a1b4dd0e7487b37edf6506330f3b1eb7bb7a
Taimoor Ali <taimoorali083@gmail.com>
2025-03-03 10:57:05 +0500

    toggle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react'
 import './App.css'
 const App = () => {
-  const [show, setShow] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   return (
     <>
-<div className='checkmate'>
+      <div className='checkmate'>
 
-      {show ? <div className='status'>Content</div> : null}
-      <button onClick={() => { setShow(false) }}>hide</button>
-      <button onClick={() => { setShow(true) }}>show</button>
-</div>
+        {toggle ? <div className='status'>Content</div> : null}
+        <button onClick={() => { setToggle(!toggle) }}>Toggle</button>
+      </div>
     </>
   )
 }
