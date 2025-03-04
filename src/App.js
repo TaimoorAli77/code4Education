a7d2b3bfb82d8a2fb5bd2f16e68678cbe7c0fb31
Taimoor Ali <taimoorali083@gmail.com>
2025-03-04 08:05:11 +0500

    toggle system

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <div className='checkmate'>
-
+        <h1>Show Hide Toggle</h1>
         {toggle ? <div className='status'>Content</div> : null}
         <button onClick={() => { setToggle(!toggle) }}>Toggle</button>
       </div>
