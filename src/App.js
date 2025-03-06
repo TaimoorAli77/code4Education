c51d62ea564e35a7ef55fd13b688eae649f97d90
Taimoor Ali <taimoorali083@gmail.com>
2025-03-06 18:58:29 +0500

    component did mount

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css'
 export default class App extends Component {
   constructor() {
     super()
+    //first this will run
     console.log("constructor")
     this.state = {
       count: 0
     }
   }
+  //third cDM will run
   componentDidMount() {
     console.log("Component Did Mount !")
   }
+  //second render will run
   render() {
     console.log("render", this.state)
     return (<>
