df2912992d967e4b9bc753b239dffe1d58835353
Taimoor Ali <taimoorali083@gmail.com>
2025-03-06 19:23:40 +0500

    Comp state  increment app

--- src/App.js
+++ src/App.js
@@ -9,18 +9,39 @@ export default class App extends Component {
       count: 0
     }
   }
-  //third cDM will run
+  //third cDM will run.. mostly libraries and api calls are called inside the componentDidMount 
+  // because it just calls once when we start the app.
   componentDidMount() {
     console.log("Component Did Mount !")
   }
   //second render will run
+
+  Increment = () => {
+    this.setState((prev) => ({
+      count: prev.count + 1
+    }))
+  }
+  IncrementSec = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
   render() {
     console.log("render", this.state)
     return (<>
       <div className='checkmate'>
         <h1 className='checkmate'>Component did mount in React </h1>
-        <button onClick={() => { this.setState({count:this.state.count + 1}) }}>Click</button>
+        <button onClick={() => { this.setState((prev) => ({ count: prev.count + 1 })) }}>Click</button>
+
+
         <h2>Counter : {this.state.count}</h2>
+        <button onClick={() => { this.setState({ count: this.state.count + 1 }) }}>Click</button>
+        <h1>Another ...</h1>
+        <button onClick={this.IncrementSec}>Click</button>
+        <h2>
+          <button onClick={this.Increment}>Click</button>
+
+        </h2>
 
       </div>
     </>
