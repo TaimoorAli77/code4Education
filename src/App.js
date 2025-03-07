3f41c9df71c18c0b75425f27cc2f5bc1013e418a
Taimoor Ali <taimoorali083@gmail.com>
2025-03-07 16:36:20 +0500

    Styles in css

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './App.css'
-import UserTotal from './UserTotal'
+import './Styles.css'
+//modules.css are used to hide classes name
+import newStyles from './NewStyle.module.css'
+import './style.scss'
 const App = () => {
-  // {/* Example 2 */ }
-  const [count, setCount] = useState(0)
-  const [total, setTotal] = useState(100)
+  const js = {
+    color: "white",
+    background: "orange"
+  }
 
-  //Example 1 (On every render, UseEffect Will run)
-  useEffect(() => {
-    console.log("After Render!")
-    // alert("UseEffect Hook")
-    // console.log("UseEffect Hook")
-    // console.log("Updating...")
-    //without dependency it will run on each render 
-  })
-  useEffect(() => {
-    console.log("After Render with []...")
-    //with dependency empty brackets [] it will run once or single render occurs 
-  }, [])
-  useEffect(() => {
-    // console.log("After Render with [count] dependency...")
-    //with dependency empty brackets [] it will run once or single render occurs 
-  }, [count])
-  useEffect(() => {
-    // console.log("After Render with [total] dependency...")
-    //with dependency empty brackets [] it will run once or single render occurs 
-  }, [total])
-  return (
+  return (<>
     <div className='App'>
-      <h1>UseEffect Hook</h1>
-      {/* Example 2 */}
-      <button onClick={() => { setCount(count + 1) }}>Update Count:</button>
-      <h3>
-        {/* {count} */}
-      </h3>
-      <h1>UseEffect With Conditions</h1>
-      {/* //Example */}
-      <button onClick={() => { setTotal(total + 1) }}>Update Total:</button>
-      <h3>
-        {/* {total} */}
-      </h3>
-      <UserTotal total={total} count={count}/>
+      <h1> Styles in React </h1>
+      <h1 style={{ color: "White", background: "Green" }}>Inline Style</h1>
+
+      <h1 className='normal'>Normal Style</h1>
+
+      <h1 style={js}>JS Style</h1>
+
+      <h1 className={newStyles.capture}>Module Style</h1>
+
+      <h1 className={"primary-scss"}> saas & <span>scss</span> Style</h1>
+
     </div>
+
+
+  </>
+
   )
 }
 
