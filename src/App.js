49d08ea159b134740da27144fd73c7a30e3e3f77
Taimoor Ali <taimoorali083@gmail.com>
2025-03-07 08:03:24 +0500

    ComponentShouldUpdate

--- src/App.js
+++ src/App.js
@@ -3,60 +3,26 @@ import './App.css'
 export default class App extends Component {
   constructor() {
     super()
-    //first this will run
-    console.log("constructor")
     this.state = {
-      name: "Taimoor",
       count: 0
     }
   }
-  componentDidUpdate(preProps, preState) {
-    // console.log("Component did Update")
-    // if ((this.state.count, preState.count)) {
-    //   alert("working")
-    //   console.log("Component did Update", preState.count, this.state.count)
-    // }
-    console.log(preState.count, this.state.count)
-    if (this.state.count<10) {
-      this.setState({ count: this.state.count + 1 })
-    }
-  }
-  //third cDM will run.. mostly libraries and api calls are called inside the componentDidMount 
-  // because it just calls once when we start the app.
-  componentDidMount() {
-    console.log("Component Did Mount !")
-  }
-  //second render will run
 
-  Increment = () => {
-    this.setState((prev) => ({
-      count: prev.count + 1
-    }))
+  shouldComponentUpdate(nextProp, nextState) {
+    console.log("Should Component Update !", this.state.count, nextState)
+    // return false
+    //   if (nextState.count < 5)   return true
+    // return false
+    if (nextState.count > 5) return true
+    return false
   }
-  IncrementSec = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
+
   render() {
-    console.log("render", this.state)
     return (<>
       <div className='checkmate'>
-        <h1 className='checkmate'>Component did mount in React </h1>
-        <button onClick={() => { this.setState((prev) => ({ count: prev.count + 1 })) }}>Click</button>
-
-
+        <h1 className='checkmate'>LifeCycle ShouldComponentUpdate in React </h1>
         <h2>Counter : {this.state.count}</h2>
         <button onClick={() => { this.setState({ count: this.state.count + 1 }) }}>Click</button>
-        <h1>Another ...</h1>
-        <button onClick={this.IncrementSec}>Click</button>
-        <h2>
-          <button onClick={this.Increment}>Click</button>
-
-        </h2>
-        <h1>Component Did Update !</h1>
-        <button onClick={() => { this.setState({ name: "Malik Taimoor Ali Awan" }) }}>Update Btn</button>
-        {this.state.name}
       </div>
     </>
     )
