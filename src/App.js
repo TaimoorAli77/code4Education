ebdfd646cc2fb6b2fa3f3692061696d911ab3f41
Taimoor Ali <taimoorali083@gmail.com>
2025-03-07 17:00:52 +0500

    React-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React from 'react'
 import './App.css'
-import './Styles.css'
-//modules.css are used to hide classes name
-import newStyles from './NewStyle.module.css'
-import './style.scss'
+import { Button, Alert } from 'react-bootstrap';
+// import Button from 'react-bootstrap/Button';
+// import Alert from 'react-bootstrap/Alert';
+
+
 const App = () => {
-  const js = {
-    color: "white",
-    background: "orange"
-  }
+
 
   return (<>
     <div className='App'>
-      <h1> Styles in React </h1>
-      <h1 style={{ color: "White", background: "Green" }}>Inline Style</h1>
-
-      <h1 className='normal'>Normal Style</h1>
-
-      <h1 style={js}>JS Style</h1>
-
-      <h1 className={newStyles.capture}>Module Style</h1>
-
-      <h1 className={"primary-scss"}> saas & <span>scss</span> Style</h1>
-
+      <h1> Bootstrap in React </h1>
+      <h3>Button</h3>
+      <Button variant="primary">Primary</Button>{"  "} <br></br>
+      <h3>Alerts</h3>
+      
+        <Alert  variant={'primary'}>
+          This is a  alert—check it out!
+        </Alert>
+      
     </div>
 
 
