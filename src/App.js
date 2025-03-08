606f088b17832cd7e4462ef287385c52cbe952de
Taimoor Ali <taimoorali083@gmail.com>
2025-03-08 17:22:51 +0500

    tabel in react

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css'
 
 const App = () => {
 
-  // Example 1
-  const arr = ["A", "B", "C", "D"]
-  //Example 2 Array of objects
-  const arrObj = [
+  const tableObj = [
     {
       id: 1,
       product: "Classy",
@@ -24,45 +21,27 @@ const App = () => {
       price: 60
     },
   ]
-  //Example 3
-
-  const fruits = ["Apple", "Mangoes", "Banana", "Oranges"]
-  const fruitItems = fruits.map((fruit, index) => <li key={index}>{fruit}</li>)
   return (<>
     <div className='App'>
-      <h1> Array With Map Function in React </h1>
-      {/* Example 1 */}
-      <h3>Example 1</h3>
-
-      <ul>
-        {arr.map((data, i) => (
-          <div index={i}>
-            <li>
-              {data}
-            </li>
-          </div>
-        ))}
-      </ul>
-
-      {/* //Example 2 */}
-      <h3>Example 2</h3>
-
-      <div>
-        {arrObj.map((data, i) => (
-          <div index={data.id}>
-            <h3>PId:{data.id}</h3>
-            <ul>
-              <li>Name:{data.product}</li>
-              <li> Price:$ {data.price}</li>
-            </ul>
-          </div>
-        ))}
-      </div>
-      {/* Example 3 */}
-      <h3>Example 3</h3>
-      <ul>
-        {fruitItems}
-      </ul>
+      <h1>Table</h1>
+      <table className='App'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Product</th>
+            <th>price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableObj.map((data, i) => {
+            return <tr key={i}>
+              <td>{data.id}</td>
+              <td>{data.product}</td>
+              <td>{data.price}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
     </div>
   </>
 
