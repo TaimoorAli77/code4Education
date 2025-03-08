76370eddd6eac47d644a579bda4a4904d6a7f8c9
Taimoor Ali <taimoorali083@gmail.com>
2025-03-08 17:16:54 +0500

    map function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
 import React from 'react'
 import './App.css'
-import { Button, Alert } from 'react-bootstrap';
-// import Button from 'react-bootstrap/Button';
-// import Alert from 'react-bootstrap/Alert';
 
 
 const App = () => {
 
+  // Example 1
+  const arr = ["A", "B", "C", "D"]
+  //Example 2 Array of objects
+  const arrObj = [
+    {
+      id: 1,
+      product: "Classy",
+      price: 20
+    },
+    {
+      id: 2,
+      product: "Modern",
+      price: 40
+    },
+    {
+      id: 3,
+      product: "New",
+      price: 60
+    },
+  ]
+  //Example 3
 
+  const fruits = ["Apple", "Mangoes", "Banana", "Oranges"]
+  const fruitItems = fruits.map((fruit, index) => <li key={index}>{fruit}</li>)
   return (<>
     <div className='App'>
-      <h1> Bootstrap in React </h1>
-      <h3>Button</h3>
-      <Button variant="primary">Primary</Button>{"  "} <br></br>
-      <h3>Alerts</h3>
-      
-        <Alert  variant={'primary'}>
-          This is a  alert—check it out!
-        </Alert>
-      
+      <h1> Array With Map Function in React </h1>
+      {/* Example 1 */}
+      <h3>Example 1</h3>
+
+      <ul>
+        {arr.map((data, i) => (
+          <div index={i}>
+            <li>
+              {data}
+            </li>
+          </div>
+        ))}
+      </ul>
+
+      {/* //Example 2 */}
+      <h3>Example 2</h3>
+
+      <div>
+        {arrObj.map((data, i) => (
+          <div index={data.id}>
+            <h3>PId:{data.id}</h3>
+            <ul>
+              <li>Name:{data.product}</li>
+              <li> Price:$ {data.price}</li>
+            </ul>
+          </div>
+        ))}
+      </div>
+      {/* Example 3 */}
+      <h3>Example 3</h3>
+      <ul>
+        {fruitItems}
+      </ul>
     </div>
-
-
   </>
 
   )
