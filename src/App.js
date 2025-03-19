7c53465588e9b4d829cb479832df9d1865d7029f
Taimoor Ali <taimoorali083@gmail.com>
2025-03-19 09:21:53 +0500

    useMemo() used to avoid unnecessary rendering  for a function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
-import ChildComponent from './ChildComponent';
+import React, { useState, useMemo } from 'react';
 
 const App = () => {
+  const [num1, setNum1] = useState(0)
+  const [num2, setNum2] = useState(0)
+
   const [count, setCount] = useState(0)
 
+  const sum = useMemo(() => {
+    console.log("Calculating Sum ...")
+    let sum = num1 + num2
+    return sum
+  }, [num1, num2])
 
-  console.log("Parent Component rendered!")
+  // console.log("Component rendered!")
   return (
     <>
-      <h1>Memo in React JS</h1>
-      <ChildComponent count={count} />
+      <h1>useMemo in React JS</h1>
+      <input type="number" value={num1} onChange={(e) => { setNum1(Number(e.target.value)) }} />
+      <input type="number" value={num2} onChange={(e) => { setNum2(Number(e.target.value)) }} />
+      <h1>sum : {sum}</h1>
       {/* if we don't pass props then it will not rerender that */}
       <button onClick={() => { setCount(count + 1) }}>Increment</button>
       <h3>
-        {/* {count} */}
+        {count}
 
       </h3>
     </>
