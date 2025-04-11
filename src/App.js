91bfd6cd0b02f71887cb29de78844e814a3ef2d1
Taimoor Ali <taimoorali083@gmail.com>
2025-04-11 16:39:23 +0500

    useRef in functional comp

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import React, { Component, createRef } from 'react';
+import React, { useRef } from 'react'
 
-class App extends Component {
-  constructor() {
-    super();
-    this.myInputRef = createRef()
+const App = () => {
+  const inputRef = useRef(null)
+  const handleBtn = () => {
+    const inputValue = inputRef.current.value;
+    alert(`value  :  ${inputValue}`)
+    console.log(`value  :  ${inputValue}`)
+    console.log(inputRef)
   }
-  // componentDidMount() {
-  //   this.myInputRef.current.value = "Taimoor Ali"
-  // }
-  handleClick = () => {
-    this.myInputRef.current.focus()
-    this.myInputRef.current.style.color = "white"
-    this.myInputRef.current.style.background = "green"
-    console.log(this.myInputRef.current.value)
-  }
-  render() {
-    console.log(this.myInputRef)
-    return (
-      <div className="App">
-        <h1>Ref In React</h1>
-
-        <input type="text" ref={this.myInputRef} />
-        <button onClick={this.handleClick}>Focus input</button>
-      </div>
-    )
-  }
-
+  return (
+    <div>
+      <input ref={inputRef} type="text" />
+      <button onClick={handleBtn}>check ref/ Get input Value</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
