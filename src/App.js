3bde5389accda8397fdbacf95668e1d9de1a126d
Taimoor Ali <taimoorali083@gmail.com>
2025-04-12 09:59:46 +0500

    uncontrolled comp use useref

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { useRef, useState } from 'react'
-import ChildComponent from './ChildComponent';
-import { Component } from 'react';
+import React, { useRef } from 'react'
 
 const App = () => {
-const[val,setVal] =  useState("Taimoot")
-const handleChange = (e)=>{
-  setVal(e.target.value)
-}
+  const inputRef = useRef(null)
+  const inputRef2 = useRef(null)
+  const inputRef3 = useRef(null)
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log("The first input Value:", inputRef.current.value)
+    console.log("The Second input value: ", inputRef2.current.value)
+    console.log("3rd val : ", inputRef3.current.value)
+    console.log("4th val : ", document.getElementById("val3").value)
+
+  }
   return (
     <div>
-      <h1 >controlled Component: states are used form and input plus watch w3school for refuse in function</h1> 
-      <input type="text" value={val} onChange={(e)=>handleChange(e)}/>
-      <h3>{val}</h3>
+      <h1 >Uncontrolled Component: useRef are used and useState are not used in form handling. Dom is used to manage
+        value. No rerenders. reacts virtual Dom and actual dom diff. through id can also be used.</h1>
+      <form onSubmit={handleChange}>
+
+        <input type="text" ref={inputRef} />
+        <input type="text" ref={inputRef2} />
+        <input type="text" ref={inputRef3} />
+        <input type='text'  id='val3'/>
+
+        <button>submit</button>
+      </form>
+
     </div>
   )
 }
