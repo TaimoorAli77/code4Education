4b2b1cab08cd8988cb7660947e11c7502f92c527
Taimoor Ali <taimoorali083@gmail.com>
2025-05-05 20:10:28 +0500

    usesyncExternalStore hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { useLayoutEffect, useRef, useState } from 'react'
+import React, { useSyncExternalStore } from 'react'
 
 const App = () => {
-  const [width, setWidth] = useState(0)
-  const elementRef = useRef(null)
-  console.log(elementRef)
-  useLayoutEffect(() => {
-    if (elementRef.current) {
-      setWidth(elementRef.current.getBoundingClientRect().width)
+
+  const width = useSyncExternalStore((listener) => {
+    window.addEventListener("resize", listener)
+    return () => {
+      window.removeEventListener("resize", listener)
     }
-  }, [])
+  }, () => window.innerWidth)
 
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>useLayoutEffect Hook </h1>
+      <h1>useSyncExternalStore Hook </h1>
 
-      <h2>Width Measurement Example</h2>
-
-      <div ref={elementRef} style={{ width: "600px", margin: 'auto', background: "lightblue" }}>Element whose width is Measured.</div>
-      <p>width : {width}px</p>
+      <h2>{width}</h2>
     </div>
   )
 }
